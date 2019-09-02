23015f5e50890e021a3459d0831311a3c8e529cf
Monica Wojciechowska <monica.wojciechowska@netguru.pl>
2019-09-02 23:41:09 +0200

    add svg loader to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,8 +50,16 @@ module.exports = {
         head: true,
       },
     },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /src/,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
