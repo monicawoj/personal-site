0eb4fad48f34c722e0fceed26ad0fc72a7626187
Monica Wojciechowska <monica.wojciechowska@netguru.pl>
2019-09-17 12:06:29 -0400

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     `gatsby-plugin-resolve-src`,
     `gatsby-plugin-remove-trailing-slashes`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
